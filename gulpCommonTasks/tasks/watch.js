0cdd9c242c163e9a5f8c9db17b678ba3a7283431
Pavel Belousov <pafnuty10@gmail.com>
2015-11-16 22:32:51 +0300

    error on first compile fix :(

--- gulpCommonTasks/tasks/watch.js
+++ gulpCommonTasks/tasks/watch.js
@@ -3,8 +3,7 @@ var runSequence = require('run-sequence');
 var config = require('../config');
 
 gulp.task('watch', function () {
-	gulp.watch(config.less.src, ['compile-less']);
-	gulp.watch([config.concatcss.src, config.inliner.src], ['build']);
+	gulp.watch([config.less.src, config.concatcss.src, config.inliner.src], ['build']);
 	gulp.watch(config.images.src, ['images']);
 });
 
