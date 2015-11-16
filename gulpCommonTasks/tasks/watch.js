6b4f5bf56fff3b012ac2b9e3258e09a2be25c023
Pavel Belousov <pafnuty10@gmail.com>
2015-11-16 22:18:40 +0300

    Move from CSS to LESS

--- gulpCommonTasks/tasks/watch.js
+++ gulpCommonTasks/tasks/watch.js
@@ -3,11 +3,12 @@ var runSequence = require('run-sequence');
 var config = require('../config');
 
 gulp.task('watch', function () {
+	gulp.watch(config.less.src, ['compile-less']);
 	gulp.watch([config.concatcss.src, config.inliner.src], ['build']);
 	gulp.watch(config.images.src, ['images']);
 });
 
-gulp.task('build', function(cb) {
+gulp.task('build', function (cb) {
 	runSequence(
 		'concatcss',
 		'inliner',
