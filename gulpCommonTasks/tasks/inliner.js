aaca54891187bba0afac66cf19121d5977bbf661
Arthur Koch <dudeonthehorse@gmail.com>
2016-02-05 05:28:05 +0300

    Template Engine Update
    - removed gulp-file-include package
    - added mozilla nunjucks template engine package
    - including files moved to partials folder
    - added watcher for partials
    - changed syntax for including files
    
    Now we can use all features of Nunjucks template engine in our projects

--- gulpCommonTasks/tasks/inliner.js
+++ gulpCommonTasks/tasks/inliner.js
@@ -1,25 +1,32 @@
 var gulp = require('gulp');
-var config  = require('../config').inliner;
+var config = require('../config').inliner;
 var inlineCss = require('gulp-inline-css');
-var fileinclude = require('gulp-file-include');
 var htmlmin = require('gulp-html-minifier');
 var notify = require("gulp-notify");
+var nunjucksRender = require('gulp-nunjucks-render');
 var plumber = require('gulp-plumber');
 
 gulp.task('inliner', function() {
-	return gulp.src(config.src, {cwd: config.cwd})
-		.pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-		.pipe(fileinclude({
-			prefix: '@@',
-			basepath: '@file'
+	nunjucksRender.nunjucks.configure([config.cwd], {
+		watch: false
+	});
+	return gulp.src(config.src, {
+			cwd: config.cwd
+		})
+		.pipe(plumber({
+			errorHandler: notify.onError("Error: <%= error.message %>")
 		}))
+		.pipe(nunjucksRender())
 		.pipe(inlineCss({
 			removeStyleTags: false
 		}))
-		.pipe(htmlmin({collapseWhitespace: true, minifyCSS: true}))
+		.pipe(htmlmin({
+			collapseWhitespace: true,
+			minifyCSS: true
+		}))
 		.pipe(notify({
-			title: 'E-mail buil',
-			message: 'Сборка проекта успешно завершена',
+			title: 'Email',
+			message: 'Build Done',
 			sound: "Pop"
 		}))
 		.pipe(gulp.dest(config.dest));
--- gulpCommonTasks/tasks/watch.js
+++ gulpCommonTasks/tasks/watch.js
@@ -5,6 +5,7 @@ var config = require('../config');
 gulp.task('watch', function () {
 	gulp.watch(config.concatcss.src, { cwd: config.concatcss.cwd }, ['build']);
 	gulp.watch(config.inliner.src, { cwd: config.inliner.cwd }, ['build']);
+	gulp.watch(config.inliner.src, { cwd: config.inliner.cwd + '/partials' }, ['build']);
 	gulp.watch(config.images.src, { cwd: config.images.cwd }, ['images']);
 });
 
